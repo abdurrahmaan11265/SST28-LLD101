041fbee31e18a2cfa803b40cb728b9016de96ca6
Mohammed Abdur Rahmaan <abdurrahmaan11265@gmail.com>
2026-02-23 01:37:00 +0530

    Ex3 complete

--- SOLID/Ex3/src/Main.java
+++ SOLID/Ex3/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
-        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
+        EligibilityEngine engine = new EligibilityEngine(
+            new FakeEligibilityStore(),
+            List.of(new DisciplinaryRule(), new CgrRule(), new AttendanceRule(), new CreditsRule())
+        );
         engine.runAndPrint(s);
     }
 }
