babd87b95d48ef66fc72488a777b847318ddb707
Mohammed Abdur Rahmaan <abdurrahmaan11265@gmail.com>
2026-02-23 02:25:05 +0530

    Ex5 complete

--- SOLID/Ex5/src/ExportResult.java
+++ SOLID/Ex5/src/ExportResult.java
@@ -1,9 +1,20 @@
 public class ExportResult {
     public final String contentType;
     public final byte[] bytes;
+    public final boolean isError;
+    public final String errorMessage;
 
     public ExportResult(String contentType, byte[] bytes) {
         this.contentType = contentType;
         this.bytes = bytes;
+        this.isError = false;
+        this.errorMessage = null;
+    }
+
+    public ExportResult(String errorMessage, boolean isError) {
+        this.contentType = "";
+        this.bytes = new byte[0];
+        this.isError = isError;
+        this.errorMessage = errorMessage;
     }
 }
--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,10 @@
 public abstract class Exporter {
-    // implied "contract" but not enforced (smell)
+    /**
+        Contract:
+        - req must not be null, title and body must not be null
+        - must always return a non-null ExportResult
+        - must never throw for any valid ExportRequest
+        - subclasses must not add stricter preconditions
+    */
     public abstract ExportResult export(ExportRequest req);
 }
