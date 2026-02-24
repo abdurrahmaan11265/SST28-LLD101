6c6e68de1a5b27b3400f74fbfc76fe03b4df879f
Mohammed Abdur Rahmaan <abdurrahmaan11265@gmail.com>
2026-02-24 15:00:03 +0530

    Ex 7 complete

--- SOLID/Ex7/src/ClassroomController.java
+++ SOLID/Ex7/src/ClassroomController.java
@@ -4,18 +4,18 @@ public class ClassroomController {
     public ClassroomController(DeviceRegistry reg) { this.reg = reg; }
 
     public void startClass() {
-        SmartClassroomDevice pj = reg.getFirstOfType("Projector");
+        Powerable pj = reg.getFirstOfType("Projector");
         pj.powerOn();
-        pj.connectInput("HDMI-1");
+        ((InputConnectable) pj).connectInput("HDMI-1");
 
-        SmartClassroomDevice lights = reg.getFirstOfType("LightsPanel");
-        lights.setBrightness(60);
+        Powerable lights = reg.getFirstOfType("LightsPanel");
+        ((BrightnessControllable) lights).setBrightness(60);
 
-        SmartClassroomDevice ac = reg.getFirstOfType("AirConditioner");
-        ac.setTemperatureC(24);
+        Powerable ac = reg.getFirstOfType("AirConditioner");
+        ((TemperatureControllable) ac).setTemperatureC(24);
 
-        SmartClassroomDevice scan = reg.getFirstOfType("AttendanceScanner");
-        System.out.println("Attendance scanned: present=" + scan.scanAttendance());
+        Powerable scan = reg.getFirstOfType("AttendanceScanner");
+        System.out.println("Attendance scanned: present=" + ((AttendanceScannable) scan).scanAttendance());
     }
 
     public void endClass() {
